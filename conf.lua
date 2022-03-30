75e2d8b4ccf519d75bb26f5739f5226fe995b90f
strinsberg <stevendkom@gmail.com>
2022-03-29 21:47:33 -0600

    Made this the branch to host the love.js pages. Helps keep my personal site smaller and this more where the project is.

--- conf.lua
+++
@@ -1,8 +0,0 @@
---Configuration
-function love.conf(t)
-    t.title = "Space Match 3000"
-    t.identity = "space_match_v3.0"
-    t.version = "11.2"
-    t.window.height = 640
-    t.window.width = 800
-end
